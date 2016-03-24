e1e23f371e1aaaf17b935f7163edb90af00ee001
Andy Knotts <arknotts@gmail.com>
2016-03-24 19:38:52 -0400

    Added checkout.js, and implemented a basic $http call to populate in stock items.

---
+++ Kata/App/checkout.js
@@ -0,0 +1,21 @@
+﻿//define the angular app
+var kataApp = angular.module('kataApp', []);
+
+//define a controller for the checkout process
+kataApp.controller('CheckoutController', ['$scope', '$http', function ($scope, $http) { //include http service for api communication
+
+    //declare itemsScanned in the scope. This will hold all items that have been scanned, and the quantity
+    $scope.itemsScanned = [];
+
+    //call API to get items in stock
+    $http.get('/api/items').then(function (result) //TODO this URL could be generated dynamically (would help with future refactoring)
+    {
+        //store them in the scope
+        $scope.itemsInStock = result.data;
+    });
+
+    $scope.scan = function (item)
+    {
+        //TODO
+    };
+}]);
