d85e7d6d8f3f0be9befeb789a7ab4b1d5a97ce9a
Andy Knotts <arknotts@gmail.com>
2016-03-23 22:00:19 -0400

    First commit. Skeleton structure added. One project used for simplicity.

---
+++ Kata/Controllers/api/ItemsController.cs
@@ -0,0 +1,25 @@
+﻿using Kata.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Kata.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        //ideally this would be stored in a database in the future
+        Item[] items = new Item[]
+        {
+            new Item() { Code = "A", Price = 1.25M },
+            new Item() { Code = "B", Price = 3.99M },
+        };
+
+        public IEnumerable<Item> GetAllItems()
+        {
+            return items;
+        }
+    }
+}
---
+++ Kata/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kata.Models
+{
+    public class Item
+    {
+        public string Code { get; set; }
+        public decimal Price { get; set; }
+    }
+}
