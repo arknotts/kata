57e178d4195033f57f198b8ebf299ff5aef88cdb
Andy Knotts <arknotts@gmail.com>
2016-03-24 19:15:46 -0400

    Added DealQuantity and DealPrice properties to Item class. Commented classes.

--- Kata/Controllers/api/ItemsController.cs
+++ Kata/Controllers/api/ItemsController.cs
@@ -8,15 +8,27 @@ using System.Web.Http;
 
 namespace Kata.Controllers
 {
+    /// <summary>
+    /// API controller that handles transactions related to items.
+    /// </summary>
     public class ItemsController : ApiController
     {
-        //ideally this would be stored in a database in the future
+        /// <summary>
+        /// The available items in stock at the store. Ideally this would be stored in a database.
+        /// </summary>
         Item[] items = new Item[]
         {
-            new Item() { Code = "A", Price = 1.25M },
+            new Item() { Code = "A", Price = 1.25M, DealQuantity = 2, DealPrice = 2M },
             new Item() { Code = "B", Price = 3.99M },
+            new Item() { Code = "C", Price = 4.0M, DealQuantity = 3, DealPrice = 10M },
+            new Item() { Code = "D", Price = 2.50M },
+            new Item() { Code = "E", Price = 0.99M, DealQuantity = 3, DealPrice = 2M },
         };
 
+        /// <summary>
+        /// Retrieves all items available.
+        /// </summary>
+        /// <returns>A list of all available items.</returns>
         public IEnumerable<Item> GetAllItems()
         {
             return items;
--- Kata/Models/Item.cs
+++ Kata/Models/Item.cs
@@ -5,9 +5,29 @@ using System.Web;
 
 namespace Kata.Models
 {
+    /// <summary>
+    /// Represents an available item at the store.
+    /// </summary>
     public class Item
     {
+        /// <summary>
+        /// The code identifying the item.
+        /// </summary>
         public string Code { get; set; }
+
+        /// <summary>
+        /// The price per unit of the item.
+        /// </summary>
         public decimal Price { get; set; }
+
+        /// <summary>
+        /// The quantity required to get the deal price.
+        /// </summary>
+        public int? DealQuantity { get; set; }
+
+        /// <summary>
+        /// The price of the deal (given enough DealQuantity).
+        /// </summary>
+        public decimal? DealPrice { get; set; }
     }
 }
